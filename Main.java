f09ff6b91c244e24c1e2067b7b32f8809e20cd6e
manisha15726 <103934828+manisha15726@users.noreply.github.com>
2022-05-03 21:32:16 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,8 +3,6 @@ class Main {
 	{
 		if (args.length > 0) {
 			System.out.println("Argument"+args[0]);
-			for (String val : args)
-				System.out.println(val);
 		}
                else
                   System.out.println("No arguments given"+args[0]);
