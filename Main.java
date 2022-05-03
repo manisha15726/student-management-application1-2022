db5dc91907a8ea4c037cf3679bdb3fa651637465
manisha15726 <103934828+manisha15726@users.noreply.github.com>
2022-05-03 21:52:06 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
-class Main {
-	public static void main(String[] args)
-	{
-		if (args.length > 0) {
-			System.out.println("Argument"+args[0]);
-		}
-               else
-                  System.out.println("No arguments given"+args[0]);
-	}
-}
+public class Main
+ {
+    public static void main(String[] args)
+      {
+        for (int i =1; i < args.length; i++)
+           {
+             System.out.println(args[0]);
+           }
+      }
+  }       
+	
+
 
        
